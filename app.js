ae52befe71e975ed16bda701b01254757fa3cbce
Andile Nqabelele <112821342+Andile-Nqabelele@users.noreply.github.com>
2023-03-08 14:25:56 +0200

    Form Validation Project First Upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let profileId = document.querySelector('.profile-id').value;
+let password = document.querySelector('.password').value;
+
+
+
+document.querySelector('button')
+.addEventListener('click', accessGrant)
+
+function accessGrant() {
+	this.createNewElement = <div></div>;
+	
+}
+
+
